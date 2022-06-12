d431d7614c6ccdd25ab109b0f534829e78064bca
kimcys <hakka711@gmail.com>
2022-06-12 15:07:56 +0800

    remove bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
             </div>
             @endif
         </div> -->
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://flowbite.com" class="flex items-center">
                     <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="E-Futbol Logo" />
