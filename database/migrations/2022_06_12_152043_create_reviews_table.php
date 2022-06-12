7872e10b267c5fcd67d3afaa1e833da8933f37fa
Addin15 <addin.utb15@gmail.com>
2022-06-12 23:35:11 +0800

    create db migrations

---
+++ database/migrations/2022_06_12_152043_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('arena_id');
+            $table->foreign('arena_id')->references('id')->on('arenas');
+            $table->unsignedBigInteger('reviewer_id');
+            $table->foreign('reviewer_id')->references('id')->on('users');
+            $table->string('comment');
+            $table->tinyInteger('star');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
